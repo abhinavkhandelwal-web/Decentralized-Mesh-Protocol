4f035ee2e49865bd8b21a6f7f0c5103a42e44674
abhinavkhandelwal-web <abhinav.khandelwal2018@gmail.com>
2026-09-23 00:11:28 +0530

    fix: connect beacon idle time to radio power save

--- include/power_manager.h
+++ include/power_manager.h
@@ -1,10 +1,19 @@
 #pragma once
 
+#include <cstdint>
+
 namespace MeshPowerManager
 {
+    using RadioPowerHook = void (*)();
+
     void init();
 
     void notify_activity();
 
-    void sleep_if_idle();
+    void set_radio_hooks(
+        RadioPowerHook suspend_hook,
+        RadioPowerHook resume_hook
+    );
+
+    void sleep_if_idle(uint32_t idle_time_ms);
 }
