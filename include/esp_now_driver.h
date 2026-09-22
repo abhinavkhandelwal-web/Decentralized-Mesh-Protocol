01b727fa627a81c2c61a3717aec93e7d60958195
abhinavkhandelwal-web <abhinav.khandelwal2018@gmail.com>
2026-09-23 00:09:55 +0530

    feat: add ESP-NOW sleep resume hooks
    Refactor EspNowDriver class to improve readability by formatting function pointers and adding suspend/resume methods.

--- include/esp_now_driver.h
+++ include/esp_now_driver.h
@@ -6,7 +6,12 @@
 
 class EspNowDriver : public IRadioDriver {
 private:
-    static void (*app_rx_callback)(const uint8_t* src_mac, const uint8_t* data, size_t len, int8_t rssi);
+    static void (*app_rx_callback)(
+        const uint8_t* src_mac,
+        const uint8_t* data,
+        size_t len,
+        int8_t rssi
+    );
 
 public:
     EspNowDriver() = default;
@@ -17,8 +22,29 @@ public:
     EspNowDriver& operator=(const EspNowDriver&) = delete;
 
     bool init() override;
-    bool send_bytes(const uint8_t* target_mac, const uint8_t* data, size_t len) override;
-    void set_rx_callback(void (*callback)(const uint8_t* src_mac, const uint8_t* data, size_t len, int8_t rssi)) override;
 
-    static void handle_raw_receive(const uint8_t* mac_addr, const uint8_t* incoming_data, int len, int8_t rssi);
+    void suspend();
+    void resume();
+
+    bool send_bytes(
+        const uint8_t* target_mac,
+        const uint8_t* data,
+        size_t len
+    ) override;
+
+    void set_rx_callback(
+        void (*callback)(
+            const uint8_t* src_mac,
+            const uint8_t* data,
+            size_t len,
+            int8_t rssi
+        )
+    ) override;
+
+    static void handle_raw_receive(
+        const uint8_t* mac_addr,
+        const uint8_t* incoming_data,
+        int len,
+        int8_t rssi
+    );
 };
