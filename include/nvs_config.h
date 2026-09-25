4fda2191705257c332d481bff5ad8acff720003c
abhinavkhandelwal-web <abhinav.khandelwal2018@gmail.com>
2026-09-25 15:43:17 +0530

    feat: add NVS configuration interface

---
+++ include/nvs_config.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <cstdint>
+
+namespace NVSConfig
+{
+    bool begin();
+
+    bool save_node_id(uint16_t node_id);
+    bool load_node_id(uint16_t& node_id);
+
+    bool save_node_name(const char* node_name);
+    bool load_node_name(char* node_name, size_t max_length);
+
+    void clear();
+}
